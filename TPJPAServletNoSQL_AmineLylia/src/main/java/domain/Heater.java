77a8b16c2d06fd3c5f9a9ab2fa50aadbd5511338
LyliaRabia <rabialylia@gmail.com>
2018-02-28 11:22:03 +0100

    modification classe Heater

--- TPJPAServletNoSQL_AmineLylia/src/main/java/domain/Heater.java
+++ TPJPAServletNoSQL_AmineLylia/src/main/java/domain/Heater.java
@@ -2,29 +2,66 @@ package domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 public class Heater {
 
+	private long id;
+	private String nom;
 	private int consoMoyenne;
-	
-	private String name;
-	
+	private Home home;
+
+	public Heater() {}
+
 	public Heater(String nom, int conso) {
-		this.name = nom;
+		this.nom = nom;
 		this.consoMoyenne = conso;
+	}
+
+	public Heater(String nom, int conso, Home home) {
+		this.nom = nom;
+		this.consoMoyenne = conso;
+		this.home = home;
+	}
+
+	@Id
+	@GeneratedValue
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
 
 	public int getConsoMoyenne() {
 		return consoMoyenne;
 	}
 
-	public void setConsoMoyenne(int consoMoyenne1) {
+	public void setConsoMoyenneWattPerH(int consoMoyenne1) {
 		this.consoMoyenne = consoMoyenne1;
+	}
+
+	@ManyToOne
+	public Home getHome() {
+		return home;
+	}
+
+	public void setHome(Home home) {
+		this.home = home;
+	}
+
+	@Override
+	public String toString() {
+		return "Heater [nom=" + nom + ", consoMoyenneWattPerH=" + consoMoyenne + "]";
 	}
 
 }
