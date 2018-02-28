548b728e157a6153cd169b27d44215d2f9a713dc
LyliaRabia <rabialylia@gmail.com>
2018-02-28 14:49:40 +0100

    class ElectronicDevice

--- TPJPAServletNoSQL_AmineLylia/src/main/java/domain/ElectronicDevice.java
+++ TPJPAServletNoSQL_AmineLylia/src/main/java/domain/ElectronicDevice.java
@@ -4,19 +4,33 @@ import javax.persistence.*;
 
 	@Entity
 	public class ElectronicDevice {
-	 
+
 		private String nom;
+		@Id
+		@GeneratedValue
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
 		private long id;
 		private int conso;
 		private Person person;
-		
+
 		public ElectronicDevice(String nom, int conso, Person person){
 			this.nom = nom;
 			this.conso = conso;
 			this.person = person;
 		}
 
-		
+		public ElectronicDevice(){
+
+		}
+
+
 		public int getConso() {
 			return conso;
 		}
@@ -24,24 +38,17 @@ import javax.persistence.*;
 		public void setConso(int conso) {
 			conso = conso;
 		}
-		
-		@Id
-		@GeneratedValue
-		public long getId() {
-			return id;
-		}
 
-		public void setId(int id) {
-			this.id = id;
-		}
-		
+
+
+
 		@ManyToOne(fetch=FetchType.LAZY)
 		@JoinColumn(name="OWNER_ID")
-		public Person getOwner(){
+		public Person getPerson(){
 			return person;
 		}
-		
-		public void setOwner(Person owner){
+
+		public void setPerson(Person owner){
 			this.person=owner;
 		}
 	}
